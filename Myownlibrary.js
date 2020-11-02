2eec51736994cbb4fedcca64218b7ccd9e5203ef
Anand9876 <73025487+Anand9876@users.noreply.github.com>
2020-11-02 21:59:03 +0530

    Add files via upload

--- Myownlibrary.js
+++ Myownlibrary.js
@@ -1,9 +1,8 @@
-function hascollided(bullet,wall){
+function hascollided(lbullet,lwall){
 bulletrightedge=bullet.x+bullet.width;
 wallrightedge=wall.x
 if(bulletrightedge>=wallrightedge){
 	return true;
 }
 return false;
-
 }
